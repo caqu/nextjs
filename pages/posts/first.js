853e47747e086a0798292016c9a006a4c60592da
Carlos Luis Quesada <carlosluisquesada@gmail.com>
2021-02-15 13:49:04 -0500

    Add sample page inside a folder

---
+++ pages/posts/first.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="container">
+        <h1 className="title">
+          Build a <a href="https://nextjs.org">PWA!</a>
+        </h1>
+    </div>
+  )
+}
