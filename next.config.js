b611daf9592a0b8f99434bd3e6eb1ea0b7c7e5f5
Carlos Luis Quesada <carlosluisquesada@gmail.com>
2021-02-15 13:46:23 -0500

    API sample

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    // Target must be serverless
+    // Target must be serverless for next-on-netlify
     target: "serverless",
 
     // https://nextjs.org/docs/api-reference/next.config.js/redirects
