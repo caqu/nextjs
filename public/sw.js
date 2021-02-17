01cfc97bca9c8cd441b31ab26da70d53cc89a1bc
Carlos Luis Quesada <carlosluisquesada@gmail.com>
2021-02-17 17:01:04 -0500

    Add cache of home page

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,16 @@
-self.addEventListener("install", function (event) {
+self.addEventListener('install', function (event) {
     console.log("Hello world from the Service Worker 🤙");
+    event.waitUntil(
+        caches.open('sw-cache').then(function (cache) {
+            return cache.add('/');
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
 });
